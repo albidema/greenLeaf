69dd4494bc7bd5ed332075bef3f39cca304d5118
Albi DEMA <albidema@live.com>
2019-08-22 23:47:46 +0200

    end 5

--- GreenLeaf-SPA/src/app/nav/nav.component.ts
+++ GreenLeaf-SPA/src/app/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
   login() {
     this.authService.login(this.model).subscribe(next => {
       console.log('logged in succesfully');
-    }, error => { console.log('Failed to login'); });
+    }, error => { console.log(error); });
   }
 
   loggedIn() {
